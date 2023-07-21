e7e94919522e93c9b4daf2a07ff93498f87d72ae
Soumyadip <soumyadipgantait2003@gmail.com>
2023-07-21 21:27:42 +0530

    update

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,27 +1,28 @@
-import { Component } from "react";
 
-import myresume from "../../assets/myresume.pdf"
 const CTA =()=> {
-  const handleClick = () => {
-    // using Java Script method to get PDF file
-    fetch('myresume.pdf').then(response => {
-        response.blob().then(blob => {
-            // Creating new object of PDF file
-            const fileURL = window.URL.createObjectURL(blob);
-            // Setting various property values
-            let alink = document.createElement('a');
-            alink.href = fileURL;
-            alink.download = 'myresume.pdf';
-            alink.click();
-        })
-    })
-}
+//   const handleClick = () => {
+//     // using Java Script method to get PDF file
+//     fetch('/assets/myresume.pdf').then(response => {
+//         response.blob().then(blob => {
+//             // Creating new object of PDF file
+//             const fileURL = window.URL.createObjectURL(blob);
+//             // Setting various property values
+//             let alink = document.createElement('a');
+//             alink.href = fileURL;
+//             alink.download = 'myresume.pdf';
+//             alink.click();
+//         })
+//     })
+// }
    
     return (
       <div className="cta">
-        <button onClick={handleClick} className="btn" download>
+        {/* <button onClick={handleClick} className="btn" download>
           Download CV
-        </button>
+        </button> */}
+        <a href="https://drive.google.com/file/d/1Uf3xzqXSgdjJmS4CJEbKwJwbMiX4jvA2/view?usp=sharing" target="__blank" className="btn" download>
+          Download CV
+        </a>
         <a className="btn btn-primary" href="#contact">
           Let's Talk
         </a>
