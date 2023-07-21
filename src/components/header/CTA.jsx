6170402c42cd5a2b879e0446fd761c06cc63f953
Soumyadip <soumyadipgantait2003@gmail.com>
2023-07-21 20:53:50 +0530

    again

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,19 +1,33 @@
 import { Component } from "react";
 
 import myresume from "../../assets/myresume.pdf"
-class CTA extends Component {
-  render() {
+const CTA =()=> {
+  const handleClick = () => {
+    // using Java Script method to get PDF file
+    fetch('myresume.pdf').then(response => {
+        response.blob().then(blob => {
+            // Creating new object of PDF file
+            const fileURL = window.URL.createObjectURL(blob);
+            // Setting various property values
+            let alink = document.createElement('a');
+            alink.href = fileURL;
+            alink.download = 'myresume.pdf';
+            alink.click();
+        })
+    })
+}
+   
     return (
       <div className="cta">
-        <a className="btn" href={myresume} download>
+        <button onClick={handleClick} className="btn" download>
           Download CV
-        </a>
+        </button>
         <a className="btn btn-primary" href="#contact">
           Let's Talk
         </a>
       </div>
     );
   }
-}
+
 
 export default CTA;
